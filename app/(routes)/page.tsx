8d05f95035d8a981e5aeb980077b3a79633f6d14
Aman <amans6963@gmail.com>
2023-12-31 14:50:27 +0530

    main div created

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-full bg-mainDivColor rounded-lg border border-borderColor"></div>
+  );
+}
